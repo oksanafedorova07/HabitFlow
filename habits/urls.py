bf834380cfd273f291c30c077fe6002eaacab2aa
oksanafedorova07 <kcenka4@gmail.com>
2025-07-08 17:37:05 +0300

    add habits

---
+++ habits/urls.py
@@ -0,0 +1,10 @@
+from rest_framework.routers import DefaultRouter
+from .views import HabitViewSet
+from django.urls import path, include
+
+router = DefaultRouter()
+router.register(r'habits', HabitViewSet, basename='habit')
+
+urlpatterns = [
+    path('', include(router.urls)),
+] 
