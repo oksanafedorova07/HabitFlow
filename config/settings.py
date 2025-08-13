1c9c32d349284b74cbd86be43c8e033ec59b6f55
oksanafedorova07 <kcenka4@gmail.com>
2025-08-13 09:58:04 +0300

    fix: linting errors

--- config/settings.py
+++ config/settings.py
@@ -26,6 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # Основные настройки
 SECRET_KEY = env.str("SECRET_KEY")
+
 DEBUG = env.bool("DEBUG", default=False)
 ALLOWED_HOSTS = env.list("ALLOWED_HOSTS", default=["127.0.0.1", "localhost"])
 
@@ -129,7 +130,7 @@ CELERY_RESULT_SERIALIZER = "json"
 
 # CORS
 CORS_ALLOW_ALL_ORIGINS = env.bool("CORS_ALLOW_ALL_ORIGINS", default=True)
-#Для продакшена (раскомментировать при необходимости):
+# Для продакшена (раскомментировать при необходимости):
 CORS_ALLOWED_ORIGINS = env.list("CORS_ALLOWED_ORIGINS", default=[])
 
 # Telegram
