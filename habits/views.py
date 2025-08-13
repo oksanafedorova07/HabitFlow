a999df7460a6774b32caa45ae99842d0809f8a2a
oksanafedorova07 <kcenka4@gmail.com>
2025-08-13 10:20:42 +0300

    add deploy.yml

--- habits/views.py
+++ habits/views.py
@@ -5,14 +5,11 @@ from rest_framework.response import Response
 from .models import Habit
 from .serializers import HabitSerializer
 
-
-
 class IsOwnerOrReadOnly(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         if request.method in permissions.SAFE_METHODS:
             return True
         return obj.user == request.user
-
 
 class HabitViewSet(viewsets.ModelViewSet):
     serializer_class = HabitSerializer
